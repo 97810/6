747ab9baa3db324dee55513a6228a2d71b37c374
97810 <130971221+97810@users.noreply.github.com>
2026-07-03 19:00:19 +0800

    Update HotkeyManager.h

--- src/HotkeyManager.h
+++ src/HotkeyManager.h
@@ -17,6 +17,7 @@ public:
 
 private:
     static LRESULT CALLBACK MouseHookProc(int code, WPARAM wparam, LPARAM lparam);
+    static LRESULT CALLBACK KeyboardHookProc(int code, WPARAM wparam, LPARAM lparam);
     bool RegisterCurrent(UINT click_key, UINT quick_key);
     bool Restore(UINT click_key, UINT quick_key);
     static bool IsMouseKey(UINT key);
@@ -26,6 +27,9 @@ private:
     UINT quick_key_ = 0;
     bool registered_ = false;
     HHOOK mouse_hook_ = nullptr;
+    HHOOK keyboard_hook_ = nullptr;
+    bool quick_key_down_ = false;
 
     static HotkeyManager* active_mouse_manager_;
+    static HotkeyManager* active_keyboard_manager_;
 };
